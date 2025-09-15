eb04fe3ad6d7fd06c7cf8798a9dfa444d471d7f4
Nathalynas <nathaly20camargo@gmail.com>
2025-09-15 18:06:35 -0300

    fix: ajuste cliente.entity

--- repara-backend/src/cliente/cliente.entity.ts
+++ repara-backend/src/cliente/cliente.entity.ts
@@ -2,21 +2,21 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Veiculo } from '../veiculo/veiculo.entity';
 import { OrdemServico } from '../ordemServico/ordemServico.entity';
 
-@Entity('clientes')
+@Entity('cliente')
 export class Cliente {
-  @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 255 })
   nome: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   telefone: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   email: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   endereco: string;
 
   @OneToMany(() => Veiculo, (veiculo) => veiculo.cliente)
