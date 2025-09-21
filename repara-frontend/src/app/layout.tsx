c4a0964d88730857f65a2d9e2c28311defe27f65
Alcides Antonio <166307092+AlcidesAT@users.noreply.github.com>
2025-09-21 15:25:44 -0300

    Adicionar integração de clientes sem remover código existente

--- repara-frontend/src/app/layout.tsx
+++ repara-frontend/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import ClientRoot from "./ClientRoot";
 import Header from "./components/header";
 import Navigation from "./components/Navigation";
+import Navigation from "./components/Navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ClientRoot>
           <Header />
+          <Navigation />
           <Navigation />
           {children}
         </ClientRoot>
